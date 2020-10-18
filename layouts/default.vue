64bacf4f9939c8f6e5151988a90e38c80d9df7c0
marahmanjs <imarahmandev@gmail.com>
2020-10-19 01:19:19 +0500

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <nuxt />
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+<section>
+  <h1>Homepage</h1>
+</section>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
